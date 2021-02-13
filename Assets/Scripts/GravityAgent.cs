494d1f526e5c83b0956ee58f94ebbb9310c95c2d
Sergey <sergei.s.tikhonov@phystech.edu>
2021-02-13 20:05:17 +0300

    Домашнее заданее №1 (фикс 1)

--- Assets/Scripts/GravityAgent.cs
+++ Assets/Scripts/GravityAgent.cs
@@ -10,7 +10,7 @@ public class GravityAgent : MonoBehaviour
     [SerializeField] private Vector3 m_Position;
     [SerializeField] private Vector3 m_Speed;
     [SerializeField] private Vector3 m_MassCentre;
-    private const float TOLERANCE = 0.1f;
+    private const float TOLERANCE = 2000f;
     void Start()
     {
         transform.position = m_Position;
@@ -19,15 +19,14 @@ public class GravityAgent : MonoBehaviour
     void FixedUpdate()
     {
         float distance = (m_MassCentre - transform.position).magnitude;
-        if (distance < TOLERANCE)
+        Vector3 dir = (m_MassCentre - transform.position).normalized;
+        Vector3 delta = m_Speed * Time.fixedDeltaTime;
+        float m_Acceleration = m_Mass * m_GravityConst / (float) Math.Pow(distance, 2);
+        if (m_Acceleration > TOLERANCE)
         {
             return;
         }
-        Vector3 dir = (m_MassCentre - transform.position).normalized;
-        Vector3 delta = m_Speed * Time.fixedDeltaTime;
-        transform.Translate(delta);
-        float m_Acceleration = m_Mass * m_GravityConst / (float) Math.Pow(distance, 2);
-        m_Position = transform.position + delta + dir * (m_Acceleration * (float) Math.Pow(Time.fixedDeltaTime, 2) / 2);
+        transform.Translate(delta + dir * (m_Acceleration * (float) Math.Pow(Time.fixedDeltaTime, 2) / 2));
         m_Speed = m_Speed + dir * (m_Acceleration * Time.fixedDeltaTime);
     }
 }
